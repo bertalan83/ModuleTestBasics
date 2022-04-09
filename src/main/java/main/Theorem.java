b0e2054888a7aa314231989bc15a7c519a0cee9b
bertalan83 <e.bertalan96@gmail.com>
2022-04-09 11:48:51 +0200

    theory done

--- src/main/java/main/Theorem.java
+++ src/main/java/main/Theorem.java
@@ -7,17 +7,38 @@ package main;
 public class Theorem {
 
     /* Milyen primitív adattípusok léteznek a Java-ban? Sorold fel őket! - 1p
-        Ide jön a válaszod...
+
+        - byte
+        - short
+        - long
+        - int
+        - float
+        - double
+        - char
+        - boolean
      */
 
 
     /* Mi a különbség a while és a do-while ciklusok között? - 2p
-        Ide jön a válaszod...
+
+        - a while ciklust akkor hasznaljuk, ha nem tudjuk pontosan hanyszor kell iteralnunk,
+            a feltetelt meg az utasitas vegrehajtasa elott ellenorzi
+        - a do-while ciklust akkor hasznaljuk, ha nem tudjuk pontosan hanyszor kell iteralnunk, DE minimum 1x
+            szeretnenk ha lefutna az utasitasunk, a feltetelt csak az utasitas vegrehajtasa utan ellenorzi
      */
 
 
     /* Mik a tömbök? (Mire használhatóak? Mikor érdemes használni őket? Hogyan hozhatjuk létre őket?) - 3p
-        Ide jön a válaszod...
+
+        - azonos adattipusu elemek tarolasara hasznalathatoak
+
+        - amikor tobb azonos adattipusu elemet szeretnenk egy konnyen hozzaferheto helyen eltarolni
+          (pl csapadek / homerseklet / szelerosseg egy adott honapban)
+
+        - a tombok deklarasanal a hosszt mindenkepp meg kell adni (ez kesobb nem valtoztathato),
+          a tomb tartalmat viszont utolag is megadhatjuk / modosithatjuk
+
+
      */
 
 }
